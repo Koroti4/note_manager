ba3cc065cb472e3ecc13a92b71f01d3a81d6b424
Koroti4 <kortich@yandex.ru>
2025-01-23 15:44:33 +0300

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,12 @@
+# Grade 1. Этап 1: Задание 1
+username = 'Алексей'
+title = 'Задание №1'
+content = 'Домашнее задание 1'
+status  = 'Активна'
+reated_date  = '18.01.2025'
+issue_date  = '18.02.2025'
+print('Имя пользователя - ', username)
+print('Название заметка - ', title, ' от ', reated_date,  ' (описание: ', content, ')')
+print('Статус - ', status, ' (выполнить до', issue_date, ')')
+
+
